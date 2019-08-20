9001cd0fd1a1057ca65a437784e69d06d3c879d2
Luan Menezes <luanmenezes99@gmail.com>
2019-08-20 00:15:57 -0300

    added jquery script for navbar

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,26 @@
-$("nav div").click(function() {
-    $("ul").slideToggle();
-    $("ul ul").css("display", "none");
+$(".fa-bars").click(function() { //selects the menu button on mobile
+    $(".firstLevelMenu").slideToggle(); //make the primary items appear
+    $(".secondLevelMenu").css("display", "none"); //avoid that secondary items items appear when the primary are clicked
 });
 
-$("ul li").click(function() {
-    $("ul ul").slideUp();
-    $(this).find('ul').slideToggle();
+
+$(".firstLevelMenu .fa-sort-desc").click(function() { //selects primary item SERVICES
+    $(".secondLevelMenu > ul").slideToggle(); //make the options appear
+    event.stopPropagation();
+    $("li li li").css("display", "none"); //avoid the inner list to collapse
+});
+
+//selects one of the secondary options
+//make the third level appear
+
+$(".secondLevelMenu .fa-sort-desc").click(function() { 
+    $(".thirdLevelMenu > ul").slideToggle(); 
+});
+
+
+$("ul li").click(function() { //um dos drops
+    $("ul ul").slideUp(); //os itens do outro drop sobem
+    $(this).find('ul').slideToggle(); //desce a lista do drop clicado
 });
 
 $(window).resize(function() {
