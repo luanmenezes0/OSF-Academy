89c42441895250a162326955711dd7038ca16bb7
Luan Menezes <luanmenezes99@gmail.com>
2019-08-22 16:54:19 -0300

    "added style to the navbar and footer"

--- scripts/indexScript.js
+++ scripts/indexScript.js
@@ -1,30 +1,20 @@
-$(".fa-bars").click(function() { //selects the menu button on mobile
-    $(".firstLevelMenu").slideToggle(); //make the primary items appear
-    $(".secondLevelMenu").css("display", "none"); //avoid that secondary items items appear when the primary are clicked
-});
-
-
-$(".firstLevelMenu .fa-sort-desc").click(function() { //selects primary item SERVICES
-    $(".secondLevelMenu > ul").slideToggle(); //make the options appear
-    event.stopPropagation();
-    $("li li li").css("display", "none"); //avoid the inner list to collapse
-});
-
-//selects one of the secondary options
-//make the third level appear
-
-$(".secondLevelMenu .fa-sort-desc").click(function() { 
-    $(".thirdLevelMenu > ul").slideToggle(); 
-});
-
-
-$("ul li").click(function() { //um dos drops
-    $("ul ul").slideUp(); //os itens do outro drop sobem
-    $(this).find('ul').slideToggle(); //desce a lista do drop clicado
-});
-
-$(window).resize(function() {
-    if($(window).width() > 768) {
-          $("ul").removeAttr('style');
-    }
-});
+$(function() {
+    // ------------------------------------------------------- //
+    // Multi Level dropdowns
+    // ------------------------------------------------------ //
+    $("ul.dropdown-menu [data-toggle='dropdown']").on("click", function(event) {
+      event.preventDefault();
+      event.stopPropagation();
+  
+      $(this).siblings().toggleClass("show");
+  
+  
+      if (!$(this).next().hasClass('show')) {
+        $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+      }
+      $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+        $('.dropdown-submenu .show').removeClass("show");
+      });
+  
+    });
+  });
